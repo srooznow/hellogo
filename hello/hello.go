753590c1c29ab1800f21d272f9c6adee32effdcd
srooznow <srooznow@cisco.com>
2015-12-30 10:11:27 -0800

    hello orld and a utils with test

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt"
+		"time"
+		"github.com/srooznow/stringutil")
+
+func main() {
+    fmt.Println("hello, world.")
+    fmt.Println(" Time is:", time.Now())
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
+
